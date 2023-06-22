229082fd1803c6478424a69cfe52eb83c4d7cc76
Serge Koudoro <skab12@gmail.com>
2023-06-22 10:35:58 -0400

    add no-local-version

--- quantconn/evaluate.py
+++ quantconn/evaluate.py
@@ -7,30 +7,32 @@ from dipy.segment.bundles import bundle_shape_similarity
 from dipy.stats.analysis import assignment_map
 from dipy.io.streamline import load_trk
 
+
 def evaluate_data(bundle1_fname, bundle2_fname, metrics_fnames, out_dir):
 
     bundle1 = load_trk(bundle1_fname, reference="same",
-                    bbox_valid_check=False).streamlines
+                       bbox_valid_check=False).streamlines
     bundle2 = load_trk(bundle2_fname, reference="same",
-                    bbox_valid_check=False).streamlines
+                       bbox_valid_check=False).streamlines
 
     static = Streamlines(set_number_of_points(bundle1, 20))
     moving = Streamlines(set_number_of_points(bundle2, 20))
-    
-    deformed_bundle2, moving_aligned, distances, match_pairs, warp_map = bundlewarp(static, moving,
+
+    deformed_bundle, moving_aligned, distances, match_pairs, warp_map = bundlewarp(static, moving,
                                                                                     alpha=0.001, beta=20)
-    
-    shape_profile, stdv = bundlewarp_shape_analysis(moving_aligned, deformed_bundle, no_disks=10,
-                                     plotting=True)
-    
+
+    shape_profile, stdv = bundlewarp_shape_analysis(
+        moving_aligned, deformed_bundle, no_disks=10, plotting=True)
+
     np.save(pjoin(out_dir, 'shape_profile.npy'), shape_profile)
     np.save(pjoin(out_dir, 'shape_profile_stdv.npy'), stdv)
-    
+
     rng = np.random.RandomState()
     clust_thr = [0]
     threshold = 2 # very strict threshold
-    
-    sm_score = bundle_shape_similarity(bundle1, bundle2, rng, clust_thr, threshold)
+
+    sm_score = bundle_shape_similarity(bundle1, bundle2, rng, clust_thr,
+                                       threshold)
     np.save(pjoin(out_dir, 'shape_similarity_score.npy'), stdv)
-    
+
     # write here BUAN profiles code
